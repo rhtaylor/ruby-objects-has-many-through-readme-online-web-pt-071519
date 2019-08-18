1b3c1b091d9b51805311ce792eb8e2c90c39790c
Randy Taylor <randyisbusy@gmail.com>
2019-08-18 02:34:43 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Customer
-  
+  attr_accessor :name, :age
+
+ @@all = []
+
+ def initialize(name, age)
+   @name = name
+   @age = age
+   @@all << self
+ end
+
+ def self.all
+   @@all
+ end
+
+ def new_meal(waiter, total, tip=0)
+   Meal.new(waiter,self, total, tip )
+ end
+
+ def meals
+   my_meals = Meal.all.find_all do |meal|
+      meal.customer == self
+    end
+  end
+  def waiters
+    my_meals = Meal.all.find_all do |waiter|
+      waiter.customer == self
+    end
+  my_meals.map{ |waiter| waiter.waiter}
+
+end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+  @@all = []
+  attr_accessor :name, :years_exp
+  def initialize(name, years_exp)
+    @name = name
+    @years_exp = years_exp
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_meal(customer, total, tip=0)
+    new_meal = Meal.new(self, customer, total, tip )
+  end
+  def meals
+    meals_served = Meal.all.find_all do |meal|
+       meal.waiter == self
+     end
+   end
+   def best_tipper
+     meals_served = Meal.all.find_all do |meal|
+        meal.waiter == self
+      end
+      xxx = meals_served.sort{ |a, b| b.tip <=> a.tip}
+      xxx[0].customer
+    end
 
 end
